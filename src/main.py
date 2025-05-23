964ed548b6f5adcd1b9df2a9f01dd9a47a651224
lleach82 <lance.r.leach@gmail.com>
2025-05-23 11:37:32 -0400

    fourth push

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
-from textnode import *
+import os
+import shutil
+
+
+def move_contents(src="../static", dest="../public"):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+
+    def recursive_copy(src_path, dest_path):
+        for item in os.listdir(src_path):
+            src_item = os.path.join(src_path, item)
+            dest_item = os.path.join(dest_path, item)
+            if os.path.isdir(src_item):
+                os.mkdir(dest_item)
+                recursive_copy(src_item, dest_item)
+            else:
+                shutil.copy(src_item, dest_item)
+                print(f"Copied file: {dest_item}")
+    recursive_copy(src, dest)
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    move_contents()
 
-main()
+if __name__ == "__main__":
+    main()
